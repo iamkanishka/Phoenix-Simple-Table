ae35da1a09cb98280aeecda3006655436581aa1a
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-10-05 20:40:44 +0530

    Added Sort Query functionality

--- lib/phx_simple_table/table_query.ex
+++ lib/phx_simple_table/table_query.ex
@@ -1,14 +1,29 @@
 defmodule PhxSimpleTable.TableQuery do
-alias PhxSimpleTable.Schema.TableSchema
+  alias PhxSimpleTable.Schema.TableSchema
 
   import Ecto.Query, warn: false
 
   alias PhxSimpleTable.Repo
   alias PhxSimpleTable.Schema.TableSchema
 
-  def list_table_data() do
-    Repo.all(TableSchema)
+  @doc """
+  Fetch table Data
+  """
+  def list_table_data(opts) do
+    # Get all Data
+    # Repo.all(TableSchema)
+
+    # get Sorted Data
+    from(m in TableSchema)
+    |> sort(opts)
+    |> Repo.all()
   end
 
+  defp sort(query, %{sort_by: sort_by, sort_dir: sort_dir})
+       when sort_by in [:id, :name, :gender, :weight] and
+              sort_dir in [:asc, :dec] do
+    order_by(query, {^sort_by, ^sort_dir})
+  end
 
+  defp sort(query, _opts), do: query
 end
